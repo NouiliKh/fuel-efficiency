89b3a4215b9c749d65e276d5bbec8c2f4fb3b83e
gomni <khalil.nouili@gmail.com>
2021-05-05 19:54:16 +0200

    update docker file

--- start_app.sh
+++ start_app.sh
@@ -5,6 +5,6 @@ docker-compose up --build -d
 
 cd ../app/
 cp .env.template .env
-docker network create --driver bridge my_netw
+docker network create --driver bridge my_network
 docker build -t fuel-efficency .
-docker run -p 3000:3000 --env-file=.env --network my_network -v /home/$USER/Desktop/here:/output  fuel-efficency
+docker run -p 3000:3000 --env-file=.env --network my_network -v $(pwd)/../output:/output  fuel-efficency
